f5732d1e0e410b7d736bdefb8d01b5f667cbde1a
dimasyusuf14 <yusufdimas02@gmail.com>
2023-10-10 09:43:22 +0700

    update tugas 1

---
+++ tugas_pertemuan1/app/Controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Contoh1 extends BaseController
+{
+    public function contoh1()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Dimas Yusuf Hidayat,Saya Tinggal Dikecamatan Tapos,Depok";
+    }
+}
